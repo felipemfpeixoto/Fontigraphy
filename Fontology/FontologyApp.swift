9da258194298673ac74274722e3dd54154498454
Felipe <felipe.m.f.peixoto@gmail.com>
2024-03-24 19:34:45 -0300

    Initial Commit

---
+++ Fontology/FontologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontologyApp.swift
+//  Fontology
+//
+//  Created by infra on 24/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FontologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
